fc1aafeee56426e171b259feea316a2fb3466b22
root <root@fadly-host.pgqef52ybbtubpmk4hiti3gdvf.gx.internal.cloudapp.net>
2022-05-26 20:03:05 +0000

    hehe

---
+++ README.md
@@ -0,0 +1,9 @@
+# fadly-api
+
+## penginstalan
+> npm install fadly-api
+>
+> npm uninstall fadly-api
+
+router get nya buat sendiri# fadly-api
+# fadly-api
---
+++ main.js
@@ -0,0 +1,39 @@
+const {
+    aiovideodl,
+    facebook,
+    instagram,
+    youtube,
+    tiktok,
+    jadwalsholat,
+    listsurah,
+    surah,
+    tafsirsurah,
+    liriklagu,
+    photooxy,
+    pinterest,
+    stickersearch,
+    shopee,
+    telesticker,
+    TelegraPh,
+    UploadFileUgu,
+    webp2mp4File
+} = require('./lib/index.js')
+
+module.exports.aiovideodl = aiovideodl
+module.exports.fbdl = facebook
+module.exports.igdl = instagram
+module.exports.ytdl = youtube
+module.exports.TiktokDownloader = tiktok
+module.exports.jadwalsholat = jadwalsholat
+module.exports.listsurah = listsurah
+module.exports.surah = surah
+module.exports.tafsirsurah = tafsirsurah
+module.exports.lirik = liriklagu
+module.exports.photooxy = photooxy
+module.exports.pinterest = pinterest
+module.exports.stickersearch = stickersearch
+module.exports.shopee = shopee
+module.exports.telesticker = telesticker
+module.exports.TelegraPh = TelegraPh
+module.exports.UploadFileUgu = UploadFileUgu
+module.exports.webp2mp4File = webp2mp4File
