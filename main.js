930f30ed958555bb57575cb052f3ff58b829f701
Fadly ID <87860947+xdlyy404@users.noreply.github.com>
2022-05-27 03:33:17 +0700

    hshe

--- main.js
+++ main.js
@@ -9,43 +9,7 @@ const {
     surah,
     tafsirsurah,
     liriklagu,
-    photooxy,const {
-        aiov    tiktok,
-     ac jadk,
-        instagram,
-        youtube,
-        tiktok,
-        jadwalsholat,
-        listsurah,
-        surah,
-        tafsirsurah,
-        liriklagu,
-        photooxy,
-        pinterest,
-        stickersearch,
-        shopee,
-        telesticker,
-        TelegraPh,
-        UploadFileUgu,
-        webp2mp4File
-} = require('./lib/index.js')
-
-module.exports.aiovideodl = aiovideodl
-module.exports.facebook = fbdl
-module.exports.instagram = igdl
-module.exports.youtube = ytdl
-module.exports.tiktok = TiktokDownloader
-module.exports.jadwalsholat = jadwalsholat
-module.exports.listsurah = listsurah
-module.exports.surah = surah
-module.exports.tafsirsurah =le.ex =rtsnterest
-module.exporopee = shoe.e
-module.exports.telesticker = telesticker
-module.exmodule.exports.telesticker = telesticker
-module.exmodule.exports.telesticker = telesticker
-module.exmodule.exports.telesticker = telesticker
-module.exports.TelegraPh = TelegreUgumodulle.exports.ploadFiile = webp2mp4FileeUgu
-module.exports.webp2mp4File = webp2mp4File
+    photooxy,
     pinterest,
     stickersearch,
     shopee,
@@ -56,15 +20,15 @@ module.exports.webp2mp4File = webp2mp4File
 } = require('./lib/index.js')
 
 module.exports.aiovideodl = aiovideodl
-module.exports.fbdl = facebook
-module.exports.igdl = instagram
-module.exports.ytdl = youtube
-module.exports.TiktokDownloader = tiktok
+module.exports.facebook = fbdl
+module.exports.instagram = igdl
+module.exports.youtube = ytdl
+module.exports.tiktok = TiktokDownloader
 module.exports.jadwalsholat = jadwalsholat
 module.exports.listsurah = listsurah
 module.exports.surah = surah
 module.exports.tafsirsurah = tafsirsurah
-module.exports.lirik = liriklagu
+module.exports.liriklagu = lirik
 module.exports.photooxy = photooxy
 module.exports.pinterest = pinterest
 module.exports.stickersearch = stickersearch
