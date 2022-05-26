04972a068cbd88ecfe381b50c6b7f3adcaa2c72f
Fadly ID <87860947+xdlyy404@users.noreply.github.com>
2022-05-27 03:20:59 +0700

    hehe

--- main.js
+++ main.js
@@ -9,7 +9,43 @@ const {
     surah,
     tafsirsurah,
     liriklagu,
-    photooxy,
+    photooxy,const {
+        aiov    tiktok,
+     ac jadk,
+        instagram,
+        youtube,
+        tiktok,
+        jadwalsholat,
+        listsurah,
+        surah,
+        tafsirsurah,
+        liriklagu,
+        photooxy,
+        pinterest,
+        stickersearch,
+        shopee,
+        telesticker,
+        TelegraPh,
+        UploadFileUgu,
+        webp2mp4File
+} = require('./lib/index.js')
+
+module.exports.aiovideodl = aiovideodl
+module.exports.facebook = fbdl
+module.exports.instagram = igdl
+module.exports.youtube = ytdl
+module.exports.tiktok = TiktokDownloader
+module.exports.jadwalsholat = jadwalsholat
+module.exports.listsurah = listsurah
+module.exports.surah = surah
+module.exports.tafsirsurah =le.ex =rtsnterest
+module.exporopee = shoe.e
+module.exports.telesticker = telesticker
+module.exmodule.exports.telesticker = telesticker
+module.exmodule.exports.telesticker = telesticker
+module.exmodule.exports.telesticker = telesticker
+module.exports.TelegraPh = TelegreUgumodulle.exports.ploadFiile = webp2mp4FileeUgu
+module.exports.webp2mp4File = webp2mp4File
     pinterest,
     stickersearch,
     shopee,
